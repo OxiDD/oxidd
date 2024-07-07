9e862d3c37a8b31c33c486d4a906b6695146b5a1
Nils Husung <husung@cs.uni-saarland.de>
2024-07-07 11:11:46 +0200

    Python: export util module

--- bindings/python/oxidd/__init__.py
+++ bindings/python/oxidd/__init__.py
@@ -5,7 +5,7 @@ A concurrent decision diagram library.
 
 import importlib.metadata
 
-__all__ = ["bcdd", "bdd", "protocols", "zbdd"]
+__all__ = ["bcdd", "bdd", "protocols", "util", "zbdd"]
 __version__ = importlib.metadata.version("oxidd")
 
-from . import bcdd, bdd, protocols, zbdd
+from . import bcdd, bdd, protocols, util, zbdd
