910f3f2c2c16d56923e15ddacffa65b0b3c42c6b
Maurice Laveaux <m.laveaux@tue.nl>
2026-05-22 15:50:00 +0200

    Added more operations, implemented DiagramRules that should not be used.

--- crates/oxidd-rules-ldd/src/lib.rs
+++ crates/oxidd-rules-ldd/src/lib.rs
@@ -5,7 +5,7 @@
 use std::{borrow::Borrow, cmp::Ordering, hash::Hash};
 
 use oxidd_core::{
-    DiagramRules, Edge, HasApplyCache, InnerNode, LevelNo, Manager, Node, ReducedOrNew, function::Function, util::{AllocResult, Borrowed}
+    ApplyCache, DiagramRules, Edge, HasApplyCache, HasLevel, InnerNode, LevelNo, Manager, Node, ReducedOrNew, function::{EdgeOfFunc, Function}, util::{AllocResult, Borrowed}
 };
 use oxidd_derive::{Countable, Function};
 
@@ -25,21 +25,21 @@ pub enum LDDTerminal {
 #[allow(missing_docs)]
 pub enum LDDOp {
     Union,
+
+    Project,
 }
 
-/// For LDDs it is essential that values are ordered.
+/// For LDDs it is essential that values are ordered and cloneable.
 trait LDDValue: Ord + Eq + Hash {}
 
-trait LDDManager: Manager<Terminal = LDDTerminal> + HasApplyCache<Self, LDDOp> {
-    type InnerNodeValue: LDDValue;
-}
+trait LDDManager: Manager<Terminal = LDDTerminal, InnerNodeValue: LDDValue, InnerNode: HasLevel> + HasApplyCache<Self, LDDOp>
+{}
 impl<M> LDDManager for M
 where
-    M: Manager<Terminal = LDDTerminal> + HasApplyCache<Self, LDDOp>,
+    M: Manager<Terminal = LDDTerminal> + HasApplyCache<M, LDDOp>,
     M::InnerNodeValue: LDDValue,
-{
-    type InnerNodeValue = M::InnerNodeValue;
-}
+    M::InnerNode: HasLevel,
+{}
 
 
 // --- Function Interface ------------------------------------------------------
@@ -65,10 +65,24 @@ impl<F: Function> LDDFunction<F> {
     }
 }
 
+impl<F: Function> LDDFunction<F>
+where
+    for<'id> F::Manager<'id>: LDDManager,
+{
+    #[inline]
+    fn union_edge<'id>(
+        manager: &<LDDFunction<F> as Function>::Manager<'id>,
+        f: EdgeOfFunc<'id, Self>,
+        g: EdgeOfFunc<'id, Self>,
+    ) -> AllocResult<EdgeOfFunc<'id, Self>> {
+        apply_union(manager, f.borrowed(), g.borrowed())
+    }
+}
+
 /// [`DiagramRules`] for list decision diagrams
 pub struct LDDRules;
 
-impl<E: Edge, N: InnerNode<E, Value = ()>> DiagramRules<E, N, LDDTerminal> for LDDRules {
+impl<E: Edge, N: InnerNode<E, Value: LDDValue>> DiagramRules<E, N, LDDTerminal> for LDDRules {
     type Cofactors<'a>
         = N::ChildrenIter<'a>
     where
@@ -78,15 +92,10 @@ impl<E: Edge, N: InnerNode<E, Value = ()>> DiagramRules<E, N, LDDTerminal> for L
     #[inline(always)]
     fn reduce<M: Manager<Edge = E, InnerNode = N>>(
         _manager: &M,
-        level: LevelNo,
-        children: impl IntoIterator<Item = E>,
+        _level: LevelNo,
+        _children: impl IntoIterator<Item = E>,
     ) -> ReducedOrNew<E, N> {
-        let mut it = children.into_iter();
-        let f_then = it.next().unwrap();
-        let f_else = it.next().unwrap();
-        debug_assert!(it.next().is_none());
-
-        ReducedOrNew::New(N::new(level, [f_then, f_else], ()), Default::default())
+        unimplemented!("Missing the value to construct a new node");
     }
 
     #[inline(always)]
@@ -100,17 +109,64 @@ impl<E: Edge, N: InnerNode<E, Value = ()>> DiagramRules<E, N, LDDTerminal> for L
     }
 }
 
+/// Return a singleton LDD that contains only the list represented by `vector`.
+fn singleton<M: LDDManager>(manager: &M, vector: &[M::InnerNodeValue]) -> AllocResult<M::Edge> {
+    let mut root =  manager.get_terminal(LDDTerminal::True).unwrap();
+
+    for val in vector.iter().rev() {
+        root = make_node(manager, val, root, manager.get_terminal(LDDTerminal::Empty).unwrap())?;
+    }
+
+    Ok(root)
+}
+
+/// Computes a meta LDD that is suitable for the [project] function from the
+/// given projection indices.
+///
+/// This function is useful to be able to cache the projection LDD instead of
+/// computing it from the projection array every time.
+// pub fn compute_proj<M: LDDManager>(storage: &mut M, proj: &[M::InnerNodeValue]) -> AllocResult<M::Edge> {
+//     // Compute length of proj.
+//     let length = match proj.iter().max() {
+//         Some(x) => *x + 1,
+//         None => 0,
+//     };
+
+//     // Convert projection vectors to meta information.
+//     let mut result: Vec<M::InnerNodeValue> = Vec::new();
+//     for i in 0..length {
+//         let included = proj.contains(&i);
+
+//         if included {
+//             result.push(1);
+//         } else {
+//             result.push(0);
+//         }
+//     }
+
+//     singleton(storage, &result)
+// }
+
 /// Recursively apply the 'union' operator to `f` and `g`
 fn apply_union<M: LDDManager>(
     manager: &M,
     f: Borrowed<M::Edge>,
     g: Borrowed<M::Edge>,
 ) -> AllocResult<M::Edge>
-where
-    <M as Manager>::InnerNodeValue: LDDValue,
 {
     if f == g {
         return Ok(manager.clone_edge(&f));
+    }
+
+    // Query apply cache
+    stat!(cache_query LDDOp::Union);
+    if let Some(res) = manager.apply_cache().get(
+        manager,
+        LDDOp::Union,
+        &[f.borrowed(), g.borrowed()],
+    ) {
+        stat!(cache_hit LDDOp::Union);
+        return Ok(res);
     }
 
     let f_node = match manager.get_node(&f) {
@@ -140,7 +196,7 @@ where
             let (f_down, f_right) = collect_children(f_node);
 
             let right = apply_union(manager, f_right, g)?;
-            make_node(manager, f_node.get_value(), f_down, right.borrowed())
+            make_node(manager, f_node.get_value(), manager.clone_edge(&f_down), right)
         }
         Ordering::Greater => {
             let (f_down, f_right) = collect_children(f_node);
@@ -148,17 +204,21 @@ where
 
             let low = apply_union(manager, f_down, g_down)?;
             let high = apply_union(manager, f_right, g_right)?;
-            make_node(manager, g_node.get_value(), low.borrowed(), high.borrowed())
+            make_node(manager, g_node.get_value(), low, high)
         }
         Ordering::Equal => {
             let (g_down, g_right) = collect_children(g_node);
 
             let right = apply_union(manager, f, g_right)?;
-            make_node(manager, g_node.get_value(), g_down, right.borrowed())
+            make_node(manager, g_node.get_value(), manager.clone_edge(&g_down), right)
         }
-    };
+    }?;
 
-    result
+    manager
+        .apply_cache()
+        .add(manager, LDDOp::Union, &[f, g], result.borrowed());
+
+    Ok(result)
 }
 
 /// Collect the two children of a binary node
@@ -178,9 +238,44 @@ fn collect_children<E: Edge, N: InnerNode<E>>(node: &N) -> (Borrowed<'_, E>, Bor
 fn make_node<M: LDDManager>(
     manager: &M,
     value: &<M as Manager>::InnerNodeValue,
-    down: Borrowed<M::Edge>,
-    right: Borrowed<M::Edge>,
+    down: M::Edge,
+    right: M::Edge,
 ) -> AllocResult<M::Edge> {
-    unreachable!("Not implemented yet");
-    // oxidd_core::LevelView::get_or_insert(&mut manager.level(0), InnerNode::new(0, [*down, *right], value.clone()))
+    oxidd_core::LevelView::get_or_insert(
+        &mut manager.level(0),
+        InnerNode::new(0, [down, right], value.clone()),
+    )
 }
+
+macro_rules! stat {
+    (call $op:expr) => {
+        let _ = $op as usize;
+        #[cfg(feature = "statistics")]
+        STAT_COUNTERS[$op as usize]
+            .calls
+            .fetch_add(1, ::std::sync::atomic::Ordering::Relaxed);
+    };
+    (cache_query $op:expr) => {
+        let _ = $op as usize;
+        #[cfg(feature = "statistics")]
+        STAT_COUNTERS[$op as usize]
+            .cache_queries
+            .fetch_add(1, ::std::sync::atomic::Ordering::Relaxed);
+    };
+    (cache_hit $op:expr) => {
+        let _ = $op as usize;
+        #[cfg(feature = "statistics")]
+        STAT_COUNTERS[$op as usize]
+            .cache_hits
+            .fetch_add(1, ::std::sync::atomic::Ordering::Relaxed);
+    };
+    (reduced $op:expr) => {
+        let _ = $op as usize;
+        #[cfg(feature = "statistics")]
+        STAT_COUNTERS[$op as usize]
+            .reduced
+            .fetch_add(1, ::std::sync::atomic::Ordering::Relaxed);
+    };
+}
+
+pub(crate) use stat;
